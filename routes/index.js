7430c5fd404399d5e1a28ab5292963b0e3b91082
Mark Essel <messel@gmail.com>
2016-03-19 06:47:16 -0400

    route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var oUrls = {
+  myHangout : 'https://hangouts.google.com/hangouts/_/g65qlb25ond2lblf2zalmeaazae',
+  GamingHangout : 'https://plus.google.com/hangouts/_/aoo7jqtiarhyhan3xppypdjlxqe',
+  BastsHangout: 'https://hangouts.google.com/hangouts/_/uo7yxhttfvdc3meotqv57w6myye',
+  ZaksHangout: 'https://plus.google.com/hangouts/_/acpyjch2yvdhbj3v5rjn2ctskue'
+};
+
+router.get('/:name', function(req, res, next) {
+  res.redirect(oUrls[req.params.name]);
 });
 
 module.exports = router;
