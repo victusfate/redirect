0378100e6004d984b29fefdebd5a3cf9fba80b47
Mark Essel <messel@gmail.com>
2016-03-19 06:52:19 -0400

    fixed

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 var oUrls = {
-  myHangout : 'https://hangouts.google.com/hangouts/_/g65qlb25ond2lblf2zalmeaazae',
+  MarksHangout : 'https://hangouts.google.com/hangouts/_/g65qlb25ond2lblf2zalmeaazae',
   GamingHangout : 'https://plus.google.com/hangouts/_/aoo7jqtiarhyhan3xppypdjlxqe',
   BastsHangout: 'https://hangouts.google.com/hangouts/_/uo7yxhttfvdc3meotqv57w6myye',
   ZaksHangout: 'https://plus.google.com/hangouts/_/acpyjch2yvdhbj3v5rjn2ctskue'
 };
 
 router.get('/:name', function(req, res, next) {
-  console.log('redirecting to ',oUrls[req.params.name],'name',req.params.name);
-  res.redirect(oUrls[req.params.name]);
+  var aUrl = oUrls[req.params.name];
+  if (aUrl !== undefined && aUrl !== null) {
+    res.redirect(oUrls[req.params.name]);  
+  }
+  else {
+    res.status(404).send('nothing');
+  }
+  
 });
 
 module.exports = router;
